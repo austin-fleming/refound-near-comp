d057bfd8af1608ee4314c2312ed0c6e69f49fcbf
Earl Potters <earl.potters@gmail.com>
2022-11-18 02:27:33 +0100

    Near Provider

--- frontend/apps/frontend/src/pages/_app.tsx
+++ frontend/apps/frontend/src/pages/_app.tsx
@@ -8,21 +8,24 @@ import { UIProvider } from "@modules/common/hooks/ui-context";
 import { CeloProvider } from "@modules/common/hooks/celo-context";
 import { AuthProvider } from "@modules/account/hooks/auth-context";
 import { AccountProvider } from "@modules/account/hooks/use-account";
+import { NearProvider } from "@modules/common/hooks/near-context";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<CeloProvider>
-			<AuthProvider>
-				<AccountProvider>
-					<UIProvider>
-						<Layout>
-							<Component {...pageProps} />
-						</Layout>
-						<Toaster position="bottom-center" />
-					</UIProvider>
-				</AccountProvider>
-			</AuthProvider>
-		</CeloProvider>
+		<NearProvider>
+			<CeloProvider>
+				<AuthProvider>
+					<AccountProvider>
+						<UIProvider>
+							<Layout>
+								<Component {...pageProps} />
+							</Layout>
+							<Toaster position="bottom-center" />
+						</UIProvider>
+					</AccountProvider>
+				</AuthProvider>
+			</CeloProvider>
+		</NearProvider>
 	);
 }
 
