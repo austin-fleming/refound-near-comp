84d6c5e0beb47ceaafda810cb25b79dcb75430f7
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-11-19 16:36:02 -0800

    retype near context

--- frontend/apps/frontend/src/modules/common/components/site-header/index.tsx
+++ frontend/apps/frontend/src/modules/common/components/site-header/index.tsx
@@ -1,16 +1,34 @@
 import { useAccount } from "@modules/account/hooks/use-account";
+import { useNear } from "@modules/common/hooks/near-context";
 import { cloin } from "@utils/styling/cloin";
 import { Sidebar } from "../sidebar";
 
 export const SiteHeader = () => {
 	const { login, logout, status, profile } = useAccount();
+	const { requestSignIn, signOut, isLoggedIn, wallet, near } = useNear();
+
+	console.log({ requestSignIn, signOut, isLoggedIn, wallet, near });
 
 	return (
 		<header className="fixed top-0 left-0 right-0 flex flex-row items-center justify-between border-b-2 border-solid h-headerTopHeight border-primary z-[5000] bg-white">
-			<h1>Refound</h1>
+			<h1 className="text-xl font-bold">Refound</h1>
 
 			<div className="flex flex-row items-center">
 				<div></div>
+				<button
+					type="button"
+					className="btn"
+					onClick={() => {
+						if (isLoggedIn) {
+							signOut();
+							return;
+						}
+
+						requestSignIn();
+					}}
+				>
+					{isLoggedIn ? "Near Disconnect" : "Near Connect"}
+				</button>
 				<button
 					type="button"
 					className={cloin("btn", status === "CONNECTING" && "loading")}
--- frontend/apps/frontend/src/modules/common/hooks/near-context/index.tsx
+++ frontend/apps/frontend/src/modules/common/hooks/near-context/index.tsx
@@ -1,56 +1,104 @@
-// Packages //
-import React, { useState, useEffect, useContext, createContext } from "react";
-import { keyStores, connect, WalletConnection } from "near-api-js";
+import type { Nullable } from "@repo/common/utils/helper-types";
+import { toast } from "@services/toast/toast";
+import type { ConnectConfig, Near } from "near-api-js";
+import { keyStores } from "near-api-js";
+import { connect, WalletConnection } from "near-api-js";
+import type { NearConfig } from "near-api-js/lib/near";
 import type { ReactNode } from "react";
+import { useMemo } from "react";
+import { useEffect } from "react";
+import { createContext, useCallback, useContext, useState } from "react";
 
-// @ts-ignore -- TODO: need to add type definitions for this https://blog.logrocket.com/how-to-use-react-context-typescript/
-const NearContext = createContext<any | undefined>(undefined);
+const NEAR_CONFIG: ConnectConfig = {
+	networkId: "testnet",
+	// keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+	nodeUrl: "https://rpc.testnet.near.org",
+	walletUrl: "https://wallet.testnet.near.org",
+	helperUrl: "https://helper.testnet.near.org",
+	// explorerUrl: "https://explorer.testnet.near.org",
+};
 
-function NearProvider({ children }: { children: ReactNode }) {
-	const [near, setNear] = useState<any>(undefined);
-	const [wallet, setWallet] = useState<any>(undefined);
+type State = {
+	near: Nullable<Near>;
+	wallet: Nullable<WalletConnection>;
+	isLoggedIn: boolean;
+	requestSignIn: () => Promise<void>;
+	signOut: () => void;
+};
+
+const initialState: State = {
+	near: undefined,
+	wallet: undefined,
+	isLoggedIn: false,
+	requestSignIn: async () => {},
+	signOut: () => {},
+};
+
+const NearContext = createContext<State>(initialState);
+
+export const useNear = () => useContext(NearContext);
+
+export const NearProvider = ({ children }: { children: ReactNode }) => {
+	const [near, setNear] = useState<State["near"]>(initialState.near);
+	const [wallet, setWallet] = useState<State["wallet"]>(initialState.wallet);
+	const [isLoggedIn, setIsLoggedIn] = useState<State["isLoggedIn"]>(initialState.isLoggedIn);
+
+	const initNear = useCallback(async () => {
+		if (!window) {
+			console.warn("Tried to init Near connection in non-browser environment.");
+			return;
+		}
+
+		const nearConnection = await connect({
+			...NEAR_CONFIG,
+			keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+		});
+
+		if (!nearConnection) {
+			toast.error("Cannot connect to Near", "near-connection");
+			return;
+		}
+
+		const walletConnection = new WalletConnection(nearConnection, null);
+
+		if (!walletConnection) {
+			toast.error("Cannot connect to Near wallet", "near-wallet");
+			return;
+		}
+
+		setNear(nearConnection);
+		setWallet(walletConnection);
+	}, []);
+
+	const requestSignIn = async () => wallet?.requestSignIn("refound.testnet", "Refound");
+
+	const signOut = () => {
+		console.log("signing out");
+		console.log(wallet);
+		console.log(near?.account);
+		wallet?.signOut();
+		console.log(wallet);
+		console.log(near?.account);
+	};
 
 	useEffect(() => {
-		async function initNear() {
-			const config = {
-				networkId: "testnet",
-				keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-				nodeUrl: "https://rpc.testnet.near.org",
-				walletUrl: "https://wallet.testnet.near.org",
-				helperUrl: "https://helper.testnet.near.org",
-				explorerUrl: "https://explorer.testnet.near.org",
-			};
-			const nearConnection = await connect(config);
-			const walletConnection = new WalletConnection(nearConnection, null);
-			setNear(nearConnection);
-			setWallet(walletConnection);
-		}
+		setIsLoggedIn(Boolean(wallet?.isSignedIn()));
+	}, [wallet, near]);
+
+	useEffect(() => {
 		initNear();
 	}, []);
 
-	const requestSignIn = async () => {
-		await wallet.requestSignIn("refound.testnet", "Refound");
-	};
-
-	const context = {
-		near,
-		wallet,
-		requestSignIn,
-	};
-
-	return (
-		<>
-			<NearContext.Provider value={context}>{children}</NearContext.Provider>
-		</>
+	const value: State = useMemo(
+		() => ({
+			near,
+			wallet,
+			isLoggedIn,
+			requestSignIn,
+			signOut,
+		}),
+		[near, wallet, isLoggedIn],
 	);
-}
 
-function useNear() {
-	const context = useContext(NearContext);
-	if (context === undefined) {
-		throw new Error("useOrbis must be used within a OrbisProvider");
-	}
-	return context;
-}
-
-export { NearProvider, useNear };
+	return <NearContext.Provider value={value}>{children}</NearContext.Provider>;
+};
