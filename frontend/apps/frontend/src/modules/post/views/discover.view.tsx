9edee30d0439a2434ed9ea4a0e957d582a70ce3b
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-11-25 16:04:31 -0800

    fix: remove double feed

--- frontend/apps/frontend/src/modules/post/views/discover.view.tsx
+++ frontend/apps/frontend/src/modules/post/views/discover.view.tsx
@@ -3,7 +3,6 @@ import type { Nullable } from "@utils/helper-types";
 import { useEffect, useState } from "react";
 import type { Post } from "../domain/post.entity";
 import { usePostContracts } from "../hooks/use-post-contracts";
-import NextLink from "next/link";
 import { LoadingPage } from "@modules/common/components/loading-page";
 import { PostCard } from "../components/post-card";
 
@@ -25,50 +24,14 @@ export const DiscoverView = () => {
 	}, [adapter]);
 
 	return (
-		<>
-			<section className="flex flex-col w-full px-contentPadding max-w-screen-lg mx-auto min-h-[101vh]">
-				<div className="grid grid-cols-1 gap-4 py-24 md:grid-cols-3">
-					{posts ? (
-						posts.map((post) => <PostCard key={post.id} post={post} />)
-					) : (
-						<LoadingPage />
-					)}
-				</div>
-			</section>
-
-			<section className="max-w-screen-lg mx-auto mt-headerTopHeight p-sitepad">
-				<h1 className="text-4xl font-bold">Feed</h1>
-			</section>
-			<section className="flex flex-col p-sitepad gap-sitepad">
+		<section className="flex flex-col w-full px-contentPadding max-w-screen-lg mx-auto min-h-[101vh]">
+			<div className="grid grid-cols-1 gap-4 py-24 md:grid-cols-3">
 				{posts ? (
-					posts.map((post) => (
-						<article className="relative" key={post.id}>
-							{post.isVerified && (
-								<span className="rounded-full badge">NGO Verified</span>
-							)}
-							<img src={post.imageLink} />
-							<pre>{JSON.stringify(post, null, "\t")}</pre>
-							<button
-								onClick={() => {
-									if (!adapter) return;
-
-									adapter.verifyPost({ id: post.id, verified: true });
-								}}
-							>
-								Verify
-							</button>
-
-							<NextLink href={`/post/${post.id}`}>
-								<a className="absolute top-0 right-0 w-full h-full">
-									<span className="sr-only">Go to Article</span>
-								</a>
-							</NextLink>
-						</article>
-					))
+					posts.map((post) => <PostCard key={post.id} post={post} />)
 				) : (
-					<p>No Posts</p>
+					<LoadingPage />
 				)}
-			</section>
-		</>
+			</div>
+		</section>
 	);
 };
